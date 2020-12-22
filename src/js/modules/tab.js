2c95c066e6a760ae09623af67bb24c507a42fcb9
Maria22 <m.ushakova22@gmail.com>
2020-12-22 20:41:47 +0100

    -add tabs selector

---
+++ src/js/modules/tab.js
@@ -0,0 +1,10 @@
+const tabs = () => {
+    function toggletabs(tabSelector, contentSelector, headerSelector) {
+        const header = document.querySelector(headerSelector),
+            contents = document.querySelectorAll(contentSelector),
+            tabs = document.querySelectorAll(tabSelector);
+
+    }
+}
+
+export default tabs;
