dce963113c0c66739c21b586a7280e288f795908
Maria22 <m.ushakova22@gmail.com>
2020-12-22 20:35:08 +0100

    -add open and close popup window

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -1,16 +1,40 @@
 const modal = () => {
     function openModal(selectorBtn, selectorPopup, selectorClose) {
 
-        const button = document.querySelector(selectorBtn),
+        const button = document.querySelectorAll(selectorBtn),
             popup = document.querySelector(selectorPopup),
             close = document.querySelector(selectorClose);
 
-        button.addEventListener('click', () => {
+        button.forEach(button => button.addEventListener('click', (e) => {
+            if (e.target) {
+                e.preventDefault();
+            }
             popup.style.display = 'block';
-        });
+            document.body.style.overflow = 'hidden';
 
+        }));
+        close.addEventListener('click', () => {
+            popup.style.display = 'none';
+            document.body.style.overflow = '';
+        });
+        popup.addEventListener('click', (e) => {
+            if (popup == e.target) {
+                popup.style.display = 'none';
+                document.body.style.overflow = '';
+            }
+        });
     }
-    console.log(3);
+
+    function openModalTimer(selectorPopup, time) {
+        setTimeout(() => {
+            const popup = document.querySelector(selectorPopup);
+            popup.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }, time)
+    }
+    openModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    openModal('.phone_link', '.popup', '.popup .popup_close');
+    openModalTimer('.popup', 3000);
 };
 
 export default modal;
